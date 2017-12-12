938c02c78d6938d98f061945c2bd6352732a9432
dianvirnadethbiag <dianvirnadethbiag@gmail.com>
2017-12-12 14:13:50 +0800

    final app

---
+++ www/js/process.js
@@ -0,0 +1,149 @@
+let process = {
+	menu:function(){
+		let content = 
+		<div><center><h1>Station of the Cross</h1>
+		<h2>By: Dian Virnadeth Paulo Biag</h2>
+			<div><a href='#' class="button button-big button-fill button-raised color-blue" onClick={process.english}>Start</a></div>	
+		</center></div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	english:function(){
+		let content = 
+		<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+
+				<a href='#' class="button button-big button-fill button-raised color-blue" onClick={process.menu}>Home</a>
+				
+				<center><h3>👇 PLAY SOUND 👇</h3><br/></center>
+				<div><center><audio controls><source src="audio/EverythingIDoIDoItForYou.mp3" type="audio/mpeg" /></audio></center></div>
+				
+				<center>
+				<h2>First Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/first.jpg"/></div>
+				</center>
+				<div><p>JESUS IS CONDEMNED TO DEATH <br/><br/>Pilate was eager to release Jesus. He knew that Jesus was innocent and had been handed over to himself as the Governor only because the Chief Priests wanted the hated Romans to take the blame for the death of Jesus in the eyes of the people. But Pilate’s strategies for finding a clever way of releasing Jesus led Him at last into the injustice he did. He was too pragmatic and self-confident a person to have or to trust the simple vision his wife tried to share with him: “Have nothing to do with that Just Man…” </p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Second Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/second.jpg"/></div>
+				</center>
+				<div><p>JESUS CARRIES HIS CROSS <br/><br/>Before leaving the place of His trial, Jesus had a heavy crossbeam laid upon Him. How painfully it must have pressed on His lacerated neck and shoulders, and arms and back, and pushed against the thorns still plaited over His head! It is surprising that He had the strength even to begin the way to Calvary.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Third Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/third.jpg"/></div>
+				</center>
+				<div><p>JESUS FALLS THE FIRST TIME <br/><br/>Station 3
+				Even a healthy man might easily be thrown off balance and toppled to the ground by a weight a heavy as a crossbeam if He tripped or twisted His ankle in the streets that Jesus had to tread on the way to Calvary. All the more so Jesus, then, who had been denied sleep and food and drink and had suffered constant physical abuse since the night before. He was on His last legs; and they were beginning to fail Him.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Fourth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/fourth.jpg"/></div>
+				</center>
+				<div><p>JESUS MEETS HIS MOTHER <br/><br/>Station 4
+				Mary the Mother of Jesus, and John and is mother Salome, Mary Magdalene and Mary the Mother of James and Jose, and many other women who followed Jesus, stationed themselves near the Pretorium to meet Jesus as soon as He was brought out so they could stay close to Him all the way to Calvary. They wanted to give Him the only thing they now could: their loving  companionship in these last cruel hours of His life. And so it was that they became His prayer partners, as Peter ad James and John himself had earlier failed to do in the Garden. They remained the prayer partners of Jesus throughout the final hours of His self-surrender to the Father for the  salvation of all peoples.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Fifth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/fifth.jpg"/></div>
+				</center>
+				<div><p>SIMON OF CYRENE HELPS JESUS TO CARRY HIS CROSS<br/><br/>We hate to be forced to do anything. Simon of Cyrene was forced by Roman soldiers to help a man condemned to crucifixion carry his crossbeam through a mocking mob to the place where He would be executed. Actually, of course, Simon was being coerced into helping Jesus save the world; and along the way to Calvary he began to realize faintly that he was helping this Man achieve something He was anxious to undergo even for His executioners and those who were mocking Him.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Sixth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/sixth.jpg"/></div>
+				</center>
+				<div><p>VERONICA WIPES THE FACE OF JESUS <br/><br/>he Gospel make no mention of such an incident on the way to Calvary. Indeed, this version of the story dates no further back than the Middle Ages. But the Veronica legend illustrates a great truth. And that is undoubtedly why it was added to, and also has maintained itself as, one of the Fourteen Stations on the way from the condemnation of Jesus to His burial. Those who help the suffering help Jesus Himself; and they will be rewarded for their compassion by receiving the Face or Person of Jesus in the face of person of the suffering the “Vera Icon” or true likeness.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Seventh Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/seventh.jpg"/></div>
+				</center>
+				<div><p>JESUS FALLS THE SECOND TIME <br/><br/>The humble oneself means to come down to earth, all the way down to the dirt; and this is what Jesus did quite literally when He fell to the ground again and again, falling down not just on all fours, but as low as possible, flat on His face. In His prayer for us, His sacrifice of Himself to God for us, the Sinless One took the posture we sinners should take before God. He did not cling to His quality with God but emptied Himself out, becoming One of us, and lower still, taking the form of the slave of all, humbling Himself obediently to death, even to death on a Cross, suffering all the degradation of such a death.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Eight Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/eight.jpg"/></div>
+				</center>
+				<div><p>JESUS MEETS THE WOMEN OF JERUSALEM<br/><br/>Jesus tells the women of Jerusalem to weep for themselves and their children rather than for Him because there is coming upon them a time so terrible that women without children will be considered blessed, a time so terrible that people will rather the hills fall on them than that it befall them, a time so terrible that present evil can be compared to it only as green wood is to dry. Some forty years later, after revolting against Rome and fighting to the death even among themselves, the children of these women would be shut up in Jerusalem by Titus, overcome by starvation and attack, their beloved city set afire, and they themselves crucified until the the Romans ran out of Wood.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Nineth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/nineth.jpg"/></div>
+				</center>
+				<div><p>JESUS FALLS THE THIRD TIME <br/><br/>Jesus tells the women of Jerusalem to weep for themselves and their children rather than for Him because there is coming upon them a time so terrible that women without children will be considered blessed, a time so terrible that people will rather the hills fall on them than that it befall them, a time so terrible that present evil can be compared to it only as green wood is to dry. Some forty years later, after revolting against Rome and fighting to the death even among themselves, the children of these women would be shut up in Jerusalem by Titus, overcome by starvation and attack, their beloved city set afire, and they themselves crucified until the the Romans ran out of Wood.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Tenth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/tenth.jpg"/></div>
+				</center>
+				<div><p>JESUS' CLOTHES ARE TAKEN AWAY <br/><br/>Jesus was stripped of His clothes of His last personal possessions, and left stark naked before being  nailed to the cross to die. This aspects of execution by crucifixion provides an exterior illustration of the essence of contemplative prayer, which is to let go of absolutely everything in surrendering to God, Holding back nothing at all. In the Garden, Jesus surrendered the last of his will; on the cross, He would surrender what remained of His life. The self-sacrifice of Jesus to His Father was always total, but now it would be consummated.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Eleventh Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/eleventh.jpg"/></div>
+				</center>
+				<div><p>JESUS IS NAILED TO THE CROSS <br/><br/>In addition to praying with faith and humility and perseverance, Jesus taught us to pray with forgiveness from, both seeking forgiveness from those we have wronged us. He had brought His gift to the altar of the cross and had no need to go anywhere else first to be reconciled with another because He alone had never wronged anyone. Now, as He stiffened at the searing pain of the nails being struck through His wrists, His thumbs curling rigidly inward, He realized that His executioners had locked Him into the final grip of death. But this only moved Him to pray for them as He taught us we must for our enemies. He did not condemn but even excused them, asking His father to forgive them because they had no real grasp of what they were doing.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Twelveth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/twelveth.jpg"/></div>
+				</center>
+				<div><p>JESUS DIES ON THE CROSS <br/><br/>If the cross was a pulpit from which Jesus preached the greatest sermon ever on God’s love for sinners, it was also a prie-dieu on which Jesus prayed Psalms by number but their first or best known line. Psalm 22:1 Reads: “My God, My God, why have you Forsaken Me?” And psalm 31:5 Reads: “Into Your hands I place My Spirit.” Jesus prayed these psalms to their perfect fulfillment because they are Psalms in which the faithful servant of God’s ultimate vindication of Him despite the persecution and suffering now heaped upon Him.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Thirteenth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/thirteenth.jpg"/></div>
+				</center>
+				<div><p>THE BODY OF JESUS IS TAKEN DOWN FROM THE CROSS <br/><br/>The rigid husk of what had been Jesus was lowered from the cross into the raised and waiting arms of John and Mary and the others. They held his body and kissed His face, But there was nothing. He was not there. Only a wretched remanat of what had been His was among them, torn and punctured, disfigured and discolored, a corpse not fit fro a good shroud and new tomb.did any of them recall His words then, “Destroy this temple, and in three days I will raise it up”?</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Fourteenth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/fourteenth.jpg"/></div>
+				</center>
+				<div><p>JESUS IS LAID IN THE TOMB <br/><br/>They quickly carried the body of Jesus to Joseph’s new tomb hewn out of rock, wrapped it in a shroud, rolled a great stone across the entrance and hurried off to observe the Sabbath rest. Only thereafter could they finish the burial. Little did they realize then that their prayers for the deliverance of Jesus had not only not gone unanswered but had already been more than answered, for their prayers were accepted with Jesus himself when Jesus, dying on the cross, surrendered His Spirit into the hands of His Father. This would be made manifest to them in the risen Jesus; and it would be made manifest to them soon, already on the third day,and to some of them, even at the crack of dawn on the third day.</p></div>
+				<p>+++</p><br/>
+
+				<center>
+				<h2>Fifteenth Station</h2>
+				<div class="col-auto tablet-auto"><img src="img/fifteenth.jpg"/></div>
+				<div><p>JESUS IS RISEN <br></br>The End</p></div>
+				</center>
+				<p>+++</p><br/>
+
+				
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	
+
+		loading:function(){
+		let content = <div>
+					Loading...
+				</div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
+}
+
+process.loading();
+setTimeout(function(){
+	process.menu();
+},1);
